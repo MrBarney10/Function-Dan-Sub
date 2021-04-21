27332487b611aba9bf3a5e4228f142978fe0babf
Wan Azim Suhaimi Bin Wan Abdul Aziz <18DDT19F1134@student.ptss.edu.my>
2021-04-21 11:16:14 +0800

    Updated

--- Form1.vb
+++ Form1.vb
@@ -1,23 +1,31 @@
 ﻿Public Class Form1
-    Private Sub btnSubmit1_Click(sender As Object, e As EventArgs) Handles btnsubmit1.Click
+
+    Private Sub S1()
         lblText1.Text = "Hello"
     End Sub
-
-    Private Sub btnSubmit2_Click(sender As Object, e As EventArgs) Handles btnSubmit2.Click
-        Dim strText As String
-        strText = txtBox1.Text
-        lblText2.Text = strText
+    Private Sub btnSubmit1_Click(sender As Object, e As EventArgs) Handles btnsubmit1.Click
+        S1()
     End Sub
 
-    Private Function btnSubmit3_Click(sender As Object, e As EventArgs) Handles btnSubmit3.Click
-        lblText3.Text = "Hello"
-        Return lblText3
-    End Function
+    Private Sub S2(ByVal strText As String)
+        lblText2.Text = strText
+    End Sub
+    Private Sub btnSubmit2_Click(sender As Object, e As EventArgs) Handles btnSubmit2.Click
+        S2(txtBox1.Text)
+    End Sub
 
-    Private Function btnSubmit4_Click(sender As Object, e As EventArgs) Handles btnSubmit4.Click
-        Dim strText2 As String
-        strText2 = txtBox2.Text
-        lblText4.Text = strText2
-        Return lblText4
+    Private Function S3()
+        Return "Hello"
     End Function
+    Private Sub btnSubmit3_Click(sender As Object, e As EventArgs) Handles btnSubmit3.Click
+        lblText3.Text = S3()
+    End Sub
+
+    Private Function S4(ByVal strText2 As String)
+        Return strText2
+    End Function
+    Private Sub btnSubmit4_Click(sender As Object, e As EventArgs) Handles btnSubmit4.Click
+        lblText4.Text = S4(txtBox2.Text)
+    End Sub
+
 End Class
